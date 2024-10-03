c0206d15137f8e7048ba8522aeda771c61ad23e0
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-10-03 20:43:24 +0200

    ToastController added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PersonCard } from '../share/interfaces/person-card';
 import { PeopleCardsService } from '../share/services/people-cards.service';
+import { ToastController } from '@ionic/angular';
+import { DataInputComponent } from '../share/components/data-input/data-input.component';
 
 @Component({
   selector: 'app-home',
@@ -10,20 +12,44 @@ import { PeopleCardsService } from '../share/services/people-cards.service';
 export class HomePage {
 
   constructor(
-    protected peopleCardsService: PeopleCardsService
-  ) {}
+    protected peopleCardsService: PeopleCardsService,
+    private toastController: ToastController,
+  ) {
+    
+  }
 
   addPerson(personCard: PersonCard) {
     this.peopleCardsService.add(personCard);
   }
 
-  alterFav(state: boolean, index: number) {
+  async alterFav(state: boolean, index: number) {
+    const message = (state) ? "Tarjeta añadida a favoritos" : "Tarjeta borrada de favoritos";
+    const favoriteToast = await this.toastController.create({
+      message: message,
+      duration: 1000,
+      position: "bottom",
+      color: (state) ? "success" : "danger",
+      header: "Info",
+      animated: true
+    });
+    favoriteToast.present();
     this.peopleCardsService.alterFav(state, index);
   }
 
-  deleteCard(index: number) {
-    if (window.confirm("¿Seguro que desa borrar a esa persona?"))
+  async deleteCard(index: number) {
+    if (window.confirm(`¿Seguro que desa borrar a esa persona?`)) {
+      const message = "Tarjeta de la persona eliminada";
+      const deletedToast = await this.toastController.create({
+        message: message,
+        duration: 1000,
+        position: "bottom",
+        color: "danger",
+        header: "Info",
+        animated: true
+      });
+      deletedToast.present();
       this.peopleCardsService.remove(index);
+    }
   }
 
 }
